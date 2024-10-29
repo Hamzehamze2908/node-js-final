7ffe6e2c8ec1b60f3aabb53124b7d3150b4c21dc
Hamzehamze2908 <HAMZE191@icloud.com>
2024-10-29 17:55:40 +0200

    index.js
    הוספה נוקודת

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ let points = [];
 
 app.use(bodyParser.json());
 app.use(express.static('client'));
+
+app.post('/api/points', (req, res) => {
+    const { name } = req.body;
+    const newPoint = { id: points.length + 1, name };
+    points.push(newPoint);
+    res.status(201).json(newPoint);
+});
